9bd55aea03b0b764ac834efa79c1e56627fc38c1
Edi Modrić <edi.modric@gmail.com>
2017-08-22 15:32:50 +0200

    Add empty controller and routes

---
+++ src/AppBundle/Controller/UrlController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use eZ\Bundle\EzPublishCoreBundle\Controller;
+
+class UrlController extends Controller
+{
+    public function listUrlsAction()
+    {
+    }
+
+    public function viewUrlAction()
+    {
+    }
+
+    public function editUrlAction()
+    {
+    }
+}
