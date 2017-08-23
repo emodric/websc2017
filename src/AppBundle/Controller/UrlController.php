165b65e5121f5a84f8fcd32ecdc0e7362aa518e9
Edi Modrić <edi.modric@gmail.com>
2017-08-23 11:27:29 +0200

    Implement list urls page

--- src/AppBundle/Controller/UrlController.php
+++ src/AppBundle/Controller/UrlController.php
@@ -3,11 +3,26 @@
 namespace AppBundle\Controller;
 
 use eZ\Bundle\EzPublishCoreBundle\Controller;
+use Pagerfanta\Adapter\DoctrineORMAdapter;
+use Pagerfanta\Pagerfanta;
+use Symfony\Component\HttpFoundation\Request;
 
 class UrlController extends Controller
 {
-    public function listUrlsAction()
+    public function listUrlsAction(Request $request)
     {
+        $em = $this->get('doctrine.orm.entity_manager');
+
+        $queryBuilder = $em->createQueryBuilder()
+            ->select('u')
+            ->from('AppBundle:Ez\Url', 'u');
+
+        $pager = new Pagerfanta(new DoctrineORMAdapter($queryBuilder));
+        $pager->setNormalizeOutOfRangePages(true);
+        $pager->setMaxPerPage(3);
+        $pager->setCurrentPage((int)$request->query->get('page', 1));
+
+        return $this->render('@App/url/list.html.twig', array('urls' => $pager));
     }
 
     public function viewUrlAction()
