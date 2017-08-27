7aa0d6f9e40a6b67efe41db26a15ec19e57abf8b
Edi Modrić <edi.modric@gmail.com>
2017-08-27 12:14:17 +0200

    Add controller that links all urls releated to a content

---
+++ src/AppBundle/Controller/ContentController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use eZ\Bundle\EzPublishCoreBundle\Controller;
+use eZ\Publish\API\Repository\Values\Content\Content;
+use eZ\Publish\API\Repository\Values\Content\Field;
+
+class ContentController extends Controller
+{
+    public function contentUrlsAction(Content $content)
+    {
+        $fieldIds = array_map(
+            function (Field $field) {
+                return $field->id;
+            },
+            $content->getFields()
+        );
+
+        $em = $this->get('doctrine.orm.entity_manager');
+
+        $contentLinks = $em->getRepository('AppBundle:Ez\UrlContentLink')
+            ->findBy(
+                array(
+                    'contentFieldId' => $fieldIds,
+                    'contentFieldVersion' => $content->getVersionInfo()->versionNo
+                )
+            );
+
+        foreach ($contentLinks as $contentLink) {
+            if (!isset($urls[$contentLink->getUrlId()])) {
+                $urls[$contentLink->getUrlId()] = $contentLink->getUrl();
+            }
+        }
+
+        return $this->render('@App/url/content_urls.html.twig', array('urls' => $urls, 'content' => $content));
+    }
+}
